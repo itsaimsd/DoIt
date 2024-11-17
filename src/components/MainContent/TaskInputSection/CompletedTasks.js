dd75e19065237d3bc366049ef9af4d7f123cdb9a
itsaimsd <madhusudantiwari2003@gmail.com>
2024-11-17 16:32:11 +0530

    leftside bar and main component

--- src/components/MainContent/TaskInputSection/CompletedTasks.js
+++ src/components/MainContent/TaskInputSection/CompletedTasks.js
@@ -1,28 +1,4 @@
-// // src/components/MainContent/TaskInputSection/CompletedTasks.js
-// import React from "react";
-// import "../../../styles/mainContent/CompletedTasks.css";
 
-// const CompletedTasks = ({ tasks, toggleComplete }) => {
-//   if (tasks.length === 0) return null;
-
-//   return (
-//     <div className="completed-tasks">
-//       <div className="completed-header">Completed</div>
-//       {tasks.map((task, index) => (
-//         <div key={index} className="task-item completed">
-//           <input
-//             type="checkbox"
-//             checked={task.completed}
-//             onChange={() => toggleComplete(index)}
-//           />
-//           <span className="task-text">{task.text}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CompletedTasks;
 
 // src/components/MainContent/TaskInputSection/CompletedTasks.js
 import React from "react";
--- src/components/MainContent/TaskInputSection/TaskList.js
+++ src/components/MainContent/TaskInputSection/TaskList.js
@@ -1,35 +1,4 @@
-// // src/components/MainContent/TaskInputSection/TaskList.js
-// import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
-// import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
-// import '../../../styles/mainContent/TaskList.css'; 
 
-// const TaskList = ({ tasks, toggleComplete, toggleFavorite }) => {
-//   return (
-//     <div className="task-list">
-//       {tasks.map((task, index) => (
-//         <div key={index} className="task-item">
-//           <input
-//             type="checkbox"
-//             checked={task.completed}
-//             onChange={() => toggleComplete(index)}
-//           />
-//           <span className={task.favorite ? "task-text favorite" : "task-text"}>
-//             {task.text}
-//           </span>
-//           <FontAwesomeIcon
-//             icon={task.favorite ? solidStar : regularStar}
-//             className="star-icon"
-//             onClick={() => toggleFavorite(index)}
-//           />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default TaskList;
 
 // src/components/MainContent/TaskInputSection/TaskList.js
 import React from "react";
