3ba015a465b7153af12976d6ae1a47fda453cf20
itsaimsd <madhusudantiwari2003@gmail.com>
2024-11-18 23:49:41 +0530

    Code update

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,54 +1,27 @@
-// // src/components/MainContent/MainContent.js
-// import React from 'react';
-// import '../../styles/MainContent.css';
+import React from "react";
+import TaskInputSection from "./TaskInputSection/TaskInputSection";
+import "../../styles/mainContent/MainContent.css";
 
-// function MainContent({ toggleRightSidebar, isLeftSidebarVisible }) {
-//   return (
-//     <div className={`main-content ${isLeftSidebarVisible ? 'shifted' : ''}`}>
-//       <h1>Main Content Area</h1>
-//       <button className="add-task-button" onClick={toggleRightSidebar}>
-//         Add Task
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default MainContent;
- 
-
-// src/components/MainContent/MainContent.js
-// import React from 'react';
-// import TaskInputSection from './TaskInputSection/TaskInputSection'; 
-// import '../../styles/mainContent/MainContent.css';
-
-
-// function MainContent({ toggleRightSidebar, isLeftSidebarVisible }) {
-//   return (
-//     <div className={`main-content ${isLeftSidebarVisible ? 'shifted' : ''}`}>
-//       <TaskInputSection toggleRightSidebar={toggleRightSidebar} />
-//     </div>
-//   );
-// }
-
-// export default MainContent;
-
-// src/components/MainContent/MainContent.js
-import React from 'react';
-import TaskInputSection from './TaskInputSection/TaskInputSection';
-import '../../styles/mainContent/MainContent.css';
-
-function MainContent({ toggleRightSidebar, isLeftSidebarVisible, tasks, toggleComplete, toggleFavorite, addTask, activeTab }) {
-  const filteredTasks = activeTab === "All Tasks"
-    ? tasks
-    : activeTab === "Today"
-    ? tasks // Implement "today" filtering logic if necessary
-    : activeTab === "Important"
-    ? tasks.filter(task => task.important)
-    : tasks; // Default fallback for other tabs
+function MainContent({
+  toggleRightSidebar,
+  isLeftSidebarVisible,
+  tasks,
+  toggleComplete,
+  toggleFavorite,
+  addTask,
+  activeTab,
+}) {
+  const filteredTasks =
+    activeTab === "All Tasks"
+      ? tasks
+      : activeTab === "Today"
+        ? tasks // Implement "today" filtering logic if necessary
+        : activeTab === "Important"
+          ? tasks.filter((task) => task.important)
+          : tasks; // Default fallback for other tabs
 
   return (
-    <div className={`main-content ${isLeftSidebarVisible ? 'shifted' : ''}`}>
-      <h1>{activeTab} Tasks</h1>
+    <div className={`main-content ${isLeftSidebarVisible ? "shifted" : ""}`}>
       <TaskInputSection
         tasks={filteredTasks}
         toggleComplete={toggleComplete}
